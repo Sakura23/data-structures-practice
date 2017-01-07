244bfc260e3d4f9fb2caf77026b721f5f5181e84
Sakura23 <Sakura23@users.noreply.github.com>
2017-01-08 10:47:39 +1100

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,11 +1,11 @@
-	public void reverse (){
-		Node prev = null;
-		Node curr = head;
-		while (curr != null){
-			Node next = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = next;
-		}
-	    head = prev;
-	}
+public void reverse (){
+    Node prev = null;
+    Node curr = head;
+    while (curr != null){
+        Node next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
